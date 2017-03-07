d70827613870579914d0dc077f1fa7bf388cdb8c
mtauteris <mindaugas.tauteris@gmail.com>
2017-03-07 14:33:39 +0200

    changed java class to interface and back

--- src/Trigonometry.java
+++ src/Trigonometry.java
@@ -1,22 +1,22 @@
 
 public class Trigonometry {
 
-	public Double sin(int x) {
+	public static Double sin(int x) {
 		return Math.sin(Math.toRadians(x));
 
 	}
 
-	public Double cos(int x) {
+	public static Double cos(int x) {
 
 		return Math.cos(Math.toRadians(x));
 
 	}
 
-	public Double tan(int x) {
+	public static Double tan(int x) {
 		return Math.tan(Math.toRadians(x));
 	}
 
-	public Double ctg(int x) {
+	public static Double ctg(int x) {
 		return 1 / tan(x);
 	}
 
